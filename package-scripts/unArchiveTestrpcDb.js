919e2a6200dc634a3688d902bbc7129ba387ae43
Doug Kent <github@douglaskent.com>
2017-12-15 07:12:15 -0500

    fixed lint errors

--- package-scripts/unArchiveTestrpcDb.js
+++ package-scripts/unArchiveTestrpcDb.js
@@ -1,9 +1,5 @@
 const decompress = require('decompress');
-
-// function unArchiveTestrpcDb() {
-  const pathDaostackArcTestrpcDbZip = process.argv[2];
-  const pathDaostackArcTestrpcDb = process.argv[3];
-  console.log(`unArchiveTestrpcDb(${pathDaostackArcTestrpcDbZip}, ${pathDaostackArcTestrpcDb}`);
-  decompress(pathDaostackArcTestrpcDbZip, pathDaostackArcTestrpcDb);
-//}
-
+const pathDaostackArcTestrpcDbZip = process.argv[2];
+const pathDaostackArcTestrpcDb = process.argv[3];
+// console.log(`unArchiveTestrpcDb(${pathDaostackArcTestrpcDbZip}, ${pathDaostackArcTestrpcDb}`);
+decompress(pathDaostackArcTestrpcDbZip, pathDaostackArcTestrpcDb);
