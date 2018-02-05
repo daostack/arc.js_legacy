ed51cf8533f6941eb891679e8aafc623bdba44d2
Doug Kent <github@douglaskent.com>
2018-02-05 14:36:55 -0500

    fixed name of getGenesisDao

--- lib/arc.d.ts
+++ lib/arc.d.ts
@@ -494,7 +494,7 @@ declare module "daostack-arc.js" {
      * Returns promise of the DAOstack (Genesis) avatar address, or undefined if not found
      * @param daoCreatorAddress - Optional address of DaoCreator to use
      */
-    static getDaoCreator(daoCreatorAddress?: string): Promise<string | undefined>
+    static getGenesisDao(daoCreatorAddress?: string): Promise<string | undefined>
     /**
      * Avatar truffle contract
      */
