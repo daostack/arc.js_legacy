3312445c3d0a5bb1536c8b450cd7f89b710aa985
Doug Kent <github@douglaskent.com>
2018-02-08 13:30:52 -0500

    lint fixes

--- package-scripts/docs/createMarkdown.js
+++ package-scripts/docs/createMarkdown.js
@@ -1,19 +1,18 @@
-var typedoc = require('typedoc');
-var typeDocApp = new typedoc.Application();
- 
+const typedoc = require("typedoc");
+const typeDocApp = new typedoc.Application();
+
 typeDocApp.exclude = ["bignumber.js"];
- 
+
 const options = typeDocApp.options.getCompilerOptions();
 Object.assign(options, {
-    "target": "es6",
-    "module": "commonjs"
+  "target": "es6",
+  "module": "commonjs"
 });
- 
+
 typeDocApp.options.setValue("includeDeclarations", true);
 // typeDocApp.options.setValue("externalPattern", "node_modules");
 typeDocApp.options.setValue("excludeExternals", true);
 typeDocApp.options.setValue("hideGenerator", true);
- 
+
 typeDocApp.generateDocs(["./lib/arc.d.ts"], "./docs");
- 
- 
+
