3f39eb46050e1d7df8e812cd71de5832eb9c0939
Oren <oren.sokolowsky@gmail.com>
2018-03-06 17:10:41 +0200

    Change normalTxGasLimit to gasLimit.runtime

--- lib/utils.ts
+++ lib/utils.ts
@@ -32,7 +32,7 @@ export class Utils {
       contract.setProvider(myWeb3.currentProvider);
       contract.defaults({
         from: Utils.getDefaultAccount(),
-        gas: Config.get("normalTxGasLimit"),
+        gas: Config.get("gasLimit.runtime"),
       });
       return contract;
     } catch (ex) {
