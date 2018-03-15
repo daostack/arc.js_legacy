7d6b9018e888daf807767c77c6e6357c673663c4
Doug Kent <github@douglaskent.com>
2018-03-14 22:42:34 -0400

    tests

--- test/utils.js
+++ test/utils.js
@@ -2,10 +2,92 @@
 import "./helpers";
 import { AbsoluteVote } from "../test-dist/contracts/absoluteVote";
 import { DefaultSchemePermissions } from "../test-dist/commonTypes";
+import { LoggingService, LogLevel } from "../test-dist/loggingService";
 import { TestWrapper } from "../test-dist/test/contracts/testWrapper";
 import { Utils } from "../test-dist/utils";
+import * as helpers from "./helpers";
 
-describe("ContractWrapperBase", () => {
+describe("Utils", () => {
+
+  it("can log something", async () => {
+    LoggingService.error("this is a message that should appear in red and not crash");
+  });
+
+  it("can get a single event in timely manner", async () => {
+
+    const dao = await helpers.forgeDao();
+
+    const uController = await (await Utils.requireContract("UController")).deployed();
+
+    LoggingService.setLogLevel(LogLevel.info);
+
+    let timeEnd;
+    let lastSchemeAddress;
+    let lastAvatarAddress;
+    let timeStart = new Date();
+
+    let event = uController.RegisterScheme({}, { fromBlock: 0, toBlock: "latest" });
+
+    await new Promise((resolve) => {
+      event.get((err, log) => {
+        if (!Array.isArray(log)) { log = [log]; }
+        timeEnd = new Date();
+        LoggingService.info(`logs returned: ${log.length}`);
+        lastSchemeAddress = log[log.length - 1].args._scheme;
+        lastAvatarAddress = log[log.length - 1].args._avatar;
+        resolve();
+      });
+    });
+
+    let elapsedTime = new Date();
+    elapsedTime.setTime(timeEnd.getTime() - timeStart.getTime());
+
+    LoggingService.info(`all events, elapsed time: ${elapsedTime.getMilliseconds()}ms`);
+
+    event = uController.RegisterScheme({}, { fromBlock: 0, toBlock: "latest" });
+
+    await new Promise((resolve) => {
+      event.get((err, log) => {
+        if (!Array.isArray(log)) { log = [log]; }
+        for (let i = 0; i < log.length; ++i) {
+          const event = log[i].args;
+          if ((event._scheme === lastSchemeAddress) &&
+            (event._avatar === lastAvatarAddress)) {
+            break;
+          }
+        }
+        LoggingService.info(`logs returned: ${log.length}`);
+        timeEnd = new Date();
+        resolve();
+      });
+    });
+
+    elapsedTime = new Date();
+    elapsedTime.setTime(timeEnd.getTime() - timeStart.getTime());
+
+    LoggingService.info(`filtered events, elapsed time: ${elapsedTime.getMilliseconds()}ms`);
+
+    timeStart = new Date();
+
+    event = uController.RegisterScheme({ "_scheme": lastSchemeAddress, "_avatar": lastAvatarAddress }, { fromBlock: 0, toBlock: "latest" });
+
+    await new Promise((resolve) => {
+      event.get((err, log) => {
+        if (!Array.isArray(log)) { log = [log]; }
+        LoggingService.info(`logs returned: ${log.length}`);
+        timeEnd = new Date();
+        resolve();
+      });
+    });
+
+    elapsedTime = new Date();
+    elapsedTime.setTime(timeEnd.getTime() - timeStart.getTime());
+
+    LoggingService.info(`one event, elapsed time: ${elapsedTime.getMilliseconds()}ms`);
+
+    LoggingService.setLogLevel(LogLevel.error);
+  });
+
   it("can call getDefaultAccount", async () => {
     const defaultAccount = accounts[0];
     const defaultAccountAsync = await Utils.getDefaultAccount();
