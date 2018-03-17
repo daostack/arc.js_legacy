33bfa7626de60ffcb613eb6b68cfae728e80cfb6
Doug Kent <github@douglaskent.com>
2018-03-16 22:48:23 -0400

    removed comment

--- package-scripts.js
+++ package-scripts.js
@@ -187,10 +187,6 @@ module.exports = {
           "nps docs.website.build",
         ),
         andPreview: series("nps docs.build", "nps docs.website.preview"),
-        /**
-         * Note everytime you do a build it creates new files that will require a
-         * git commit.
-         */
         andDeploy: series("nps docs.build", "nps docs.website.deploy")
       }
     }
