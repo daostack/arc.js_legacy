b13c4fc530450606a1a6d107f8c17237c8a5b53a
Doug Kent <github@douglaskent.com>
2018-04-12 19:55:15 -0400

    enhanced and fixed InitializeArcJs

--- truffle.js
+++ truffle.js
@@ -1,32 +1,32 @@
 module.exports = {
   networks: {
     live: {
-      host: "localhost",
+      host: "127.0.0.1",
       port: 8546,
       network_id: "1",
       gas: 4543760
     },
     ganache: {
-      host: "localhost",
+      host: "127.0.0.1",
       port: 8545,
       network_id: "*",
       gas: 4543760
     },
     ropsten: {
-      host: "localhost",
+      host: "127.0.0.1",
       port: 8548,
       network_id: "3",
       gas: 4543760
     },
     kovan: {
-      host: "localhost",
+      host: "127.0.0.1",
       port: 8547,
       network_id: "42",
       gas: 4543760
     }
   },
   rpc: {
-    host: "localhost",
+    host: "127.0.0.1",
     port: 8545
   }
 };
