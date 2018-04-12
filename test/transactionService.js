52269ed0e3b6f663165d626c01b5934baaa96a5e
Doug Kent <github@douglaskent.com>
2018-04-12 11:45:12 -0400

    fix test error

--- test/transactionService.js
+++ test/transactionService.js
@@ -6,33 +6,37 @@ describe("TransactionService", () => {
   it("can get tx events from DAO.new", async () => {
 
     let txCount = 0;
+    let subscription;
 
-    const subscription = TransactionService.subscribe("txReceipts.DAO.new", (topic, txEventInfo) => {
-      assert.equal(topic, "txReceipts.DAO.new");
-      assert.isOk(txEventInfo.options);
-      assert.isOk(txEventInfo.options.schemes);
-      assert.equal(txEventInfo.options.schemes.length, 2);
-      assert.equal(txEventInfo.txCount, 6);
-      assert((txCount > 0) || txEventInfo.tx === null);
-      assert((txCount === 0) || txEventInfo.tx !== null);
-      ++txCount;
-    });
+    try {
+      subscription = TransactionService.subscribe("txReceipts.DAO.new", (topic, txEventInfo) => {
+        assert.equal(topic, "txReceipts.DAO.new");
+        assert.isOk(txEventInfo.options);
+        assert.isOk(txEventInfo.options.schemes);
+        assert.equal(txEventInfo.options.schemes.length, 2);
+        assert.equal(txEventInfo.txCount, 6);
+        assert((txCount > 0) || txEventInfo.tx === null);
+        assert((txCount === 0) || txEventInfo.tx !== null);
+        ++txCount;
+      });
 
-    await helpers.forgeDao({
-      schemes: [
-        { name: "SchemeRegistrar" },
-        {
-          name: "UpgradeScheme",
-          votingMachineParams: {
-            votePerc: 30,
-            ownerVote: true
-          }
-        },
-      ]
-    });
+      await helpers.forgeDao({
+        schemes: [
+          { name: "SchemeRegistrar" },
+          {
+            name: "UpgradeScheme",
+            votingMachineParams: {
+              votePerc: 30,
+              ownerVote: true
+            }
+          },
+        ]
+      });
+    }
+    finally {
+      subscription.unsubscribe();
+    }
 
-    assert.equal(txCount, 7);
-
-    subscription.unsubscribe();
+    assert.equal(txCount, 6);
   });
 });
