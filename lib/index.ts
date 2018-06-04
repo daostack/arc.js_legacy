1e1c3034650a301a7baec26dfcb6b94d2c813682
Doug Kent <github@douglaskent.com>
2018-06-04 17:49:36 -0400

    fix export of computeGasLimit

--- lib/index.ts
+++ lib/index.ts
@@ -28,7 +28,7 @@ export * from "./eventService";
 export * from "./loggingService";
 export * from "./transactionService";
 export * from "./utils";
-export { computeGasLimit } from "../gasLimits.js";
+export const computeGasLimit: any = require("gasLimits.js").computeGasLimit;
 
 import { Web3 } from "web3";
 import { ConfigService } from "./configService";
