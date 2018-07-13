09180293ca4a2e7087502e96a58ea17e6080f4e0
Doug Kent <github@douglaskent.com>
2018-07-12 23:09:28 -0400

    enable createGenesisDao to detect truffle-hdwallet-provider

--- package-scripts/createGenesisDao.js
+++ package-scripts/createGenesisDao.js
@@ -1,6 +1,36 @@
 const GenesisDaoCreator = require("../dist/scripts/createGenesisDao.js").GenesisDaoCreator;
 const Utils = require("../dist/utils.js").Utils;
 const env = require("env-variable")();
+let provider;
+
+const setupForNonGanacheNet = () => {
+  const webConstructor = require("web3");
+
+  let providerConfig;
+
+  console.log(`providerConfig at: ${env.arcjs_providerConfig}`);
+  providerConfig = require(env.arcjs_providerConfig);
+
+  const HDWalletProvider = require("truffle-hdwallet-provider");
+  console.log(`Provider: '${providerConfig.providerUrl}'`);
+  console.log(`Account: '${providerConfig.mnemonic}'`);
+  provider = new HDWalletProvider(providerConfig.mnemonic, providerConfig.providerUrl);
+  // Utils.getWeb3() will use this
+  global.web3 = new webConstructor(provider);
+};
+
+const exit = () => {
+  if (provider) {
+    console.log("stopping provider engine...");
+    // see: https://github.com/trufflesuite/truffle-hdwallet-provider/issues/46
+    provider.engine.stop();
+  }
+};
+
+if (env.arcjs_providerConfig) {
+  // note this can be ganache too
+  setupForNonGanacheNet();
+}
 
 Utils.getWeb3()
   .then((web3) => {
@@ -11,12 +41,15 @@ Utils.getWeb3()
       .then((daoCreationState) => {
         return createGenesisDao.setSchemes(daoCreationState).then(() => {
           console.log(`Successfully created ${daoCreationState.orgName}`);
+          exit();
         })
           .catch((ex) => {
             console.log(`Error setting schemes: ${daoCreationState.orgName}: ${ex}`);
+            exit();
           });
       })
       .catch((ex) => {
         console.log(`Error forging org: ${ex}`);
+        exit();
       });
   });
